4e9c04ebc7bbbd0a9455b6269e414a39a363f8a9
Igor Tsurkin <igor.tsurkin@exposedata.com.au>
2023-09-14 16:38:24 +1000

    chart ready

--- app/app.py
+++ app/app.py
@@ -1,15 +1,67 @@
+import plotly.graph_objects as go
+import pandas as pd
 import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html, Input, Output, State, callback_context
+from helpers_data import (
+    df_renamer, df_dropper,
+    df_persona_stream_cleaner, df_melt_ratings
+)
+from helpers_plotly import (
+    px_radar
+)
 
 # app set-up
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
-app.title = 'eMAS template builder'
+app.title = 'expose skills matrix'
 server = app.server
+
+# load the data
+df = (
+    pd.read_excel('data/Expose_Skills_Matrix_Master_Current.xlsx', sheet_name='Ratings')
+    .pipe(df_renamer)
+    .pipe(df_dropper)
+    .pipe(df_persona_stream_cleaner)
+    .pipe(df_melt_ratings)
+)
+
+print(df.head())
+
+# dropdowns
+person_selector = dbc.Select(id='person_selector', options=df['platform_area_categories'].unique())
+person_selector_2 = dbc.Select(id='person_selector_2', options=df['consultant_name'].unique())
+
+# skills graph
+test_graph = dcc.Graph(
+    id='my_graph',
+    config={
+        'displayModeBar': False,
+        'scrollZoom' : False,
+        'staticPlot' : True
+        },
+    # figure=px_radar()
+)
+
 
 # application layout
 app.layout = dbc.Container([
-    html.H1('hello world')
+    test_graph,
+    person_selector,
+    person_selector_2
 ], fluid=True)
+
+@app.callback(
+    Output('my_graph', 'figure'),
+    Input('person_selector', 'value'),
+    Input('person_selector_2', 'value')
+)
+def update_graph(person_selector_value, person_select_value_2):
+    data = (
+        df
+        .query('platform_area_categories == @person_selector_value')
+        .query('consultant_name == @person_select_value_2')
+        .query('skill_rating > 0')
+    )
+    return px_radar(data, r='skill_rating', theta='technology')
 
 # uncomment below for development and debugging
 if __name__ == '__main__':
